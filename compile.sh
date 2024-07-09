e8c2d09a4b54bd09e512e73ac83ba2fd01c43769
Eswar <eswar07519@gmail.com>
2024-07-09 17:19:44 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-riscv64-unknown-linux-gnu-gcc -march=rv64imafd -mabi=lp64d --static -DUSE_CPML black_scholes.c -o black_schole /usr/riscv_gnu_pnu_12_2_compiler_binaries/sysroot/usr/lib/libcpml.a
+riscv64-unknown-linux-gnu-gcc -march=rv64imafd -mabi=lp64d --static -DUSE_CPML black_scholes_app.c -o black_schole /usr/riscv_gnu_pnu_12_2_compiler_binaries/sysroot/usr/lib/libcpml.a
